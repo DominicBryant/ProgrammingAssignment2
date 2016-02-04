66c52327e67dd06aa44a7fe5d6c9b3fb704a1c0e
Dominic <dominic.bryant@uq.edu.au>
2016-02-04 15:11:57 +1000

    completed r programming assighnment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This set of functions create a cached matrix that can be inversed,
+##by creating two main functions (makeCacheMatrix and cacheSolve).
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The funtion makeCacheMatrix creates a matrix object that can be inversed 
+## by looping 4 nested functions (set, get, setinverse and getinverse). 
 
+makeCacheMatrix <- function (x = matrix()) {             
+        inv <- NULL                                     
+        set <-function (y){                              
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function () x
+        setinverse <- function(solve) inv <<- solve
+        getinverse <- function () inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
+## The function cacheSolve uses the functions created in makeCacheMatrix 
+## to return a matrix that is  in the inverse of x. If the inverse of the
+## matrix has already been computed, a messgae "getting cached data" will
+## be returned along with the inverted matrix.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x, ...) {
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
+
+## for example
+
+dom_matrix <- makeCacheMatrix(matrix(1:6, 2,2))
+dom_matrix$get()
+##      [,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+
+cacheSolve(dom_matrix)
+
+##      [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+
+cacheSolve(dom_matrix)
+ ##getting cached data
+   ##      [,1] [,2]
+   ##[1,]   -2  1.5
+   ##[2,]    1 -0.5
+
